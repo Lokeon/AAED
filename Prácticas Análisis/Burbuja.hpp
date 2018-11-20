e8a89eb8ee62cd12dde0e8641193b3bd21ae6bb3
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-20 17:53:50 +0100

    :penguin:

--- Prácticas Análisis/Burbuja.hpp
+++ Prácticas Análisis/Burbuja.hpp
@@ -4,7 +4,7 @@
 template <typename T>
 void burbujeo(std::vector<T>& A) {
     for(int i = 1 ; i < A.size() ; ++i) 
-        for(int j = 0 ; j < A.size() -i - 1 ; ++i) 
+        for(int j = 0 ; j < A.size() - 1 ; ++i) 
             if(A[j] > A[j+1]) 
                 std::swap(A[j],A[j+1]);
 }
