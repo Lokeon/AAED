1f2ed32f615ed4edef1c1517004f60bd2d39d81a
lokeon <kevin.lopezcala99@gmail.com>
2019-01-30 19:16:51 +0100

    TADs :penguin:

--- TADS/TadHospital.hpp
+++ TADS/TadHospital.hpp
@@ -3,131 +3,157 @@
 #include <string>
 #include <iostream>
 
-
 class Paciente
 {
-    public:
+  public:
+    Paciente(std::string n, int id, int gra) : n_(n), id_(id), gr_(gra) {}
+    std::string nombre() const { return n_; }
+    int id() const { return id_; }
+    int gravedad() const { return gr_; }
 
-    Paciente(std::string n , int id, int gra):n_(n),id_(id),gr_(gra){}
-    std::string nombre() const { return n_ ; }
-    int gravedad() const { return gr_ ; }
-    bool operator == ( const Paciente& X){ return X.id_ == id_ ;}
-    
-    private:
-    int id_,gr_;
-    std::string n_ ; 
+  private:
+    int id_, gr_;
+    std::string n_;
 };
 
 class Hospital
 {
-    public:
-
-    explicit Hospital(size_t p,size_t u,size_t m): 
-         PLANTA(Lista<Paciente>(p)) , UCI(Lista<Paciente>(u)) , MORGUE(Lista<Paciente>(m)) {}
-    void hospital(const Paciente& x);
-    void alta(const Paciente& x);
+  public:
+    explicit Hospital(size_t p, size_t u, size_t m) : PLANTA(Lista<Paciente>(p)),
+                                                      UCI(Lista<Paciente>(u)),
+                                                      MORGUE(Lista<Paciente>(m)),
+                                                      uci_(u), planta_(p), morgue_(m) {}
+    void hospital(const Paciente &x);
+    void alta(const Paciente &x);
     void muerte();
-    void UCIS() const; 
-    void PLANTAS() const ; 
-    void gravedad_determinada(int) const ; 
-   
+    void UCIS() const;
+    void PLANTAS() const;
+    void gravedad_determinada(int h) const;
+
     ~Hospital() = default;
 
-    private:
-    
+  private:
     Lista<Paciente> PLANTA;
     Lista<Paciente> UCI;
     Lista<Paciente> MORGUE;
+    size_t uci_, planta_, morgue_;
+    bool uciLLena();
+    bool plantaLLena();
+    bool morgueLLena();
 };
 
-void Hospital::hospital(const Paciente& p)
+void Hospital::hospital(const Paciente &p)
 {
-    if( (p.gravedad() > 0 && p.gravedad() < 6) && !UCI.llena())
-            UCI.insertar(p,UCI.fin());
-    else 
-        if ((p.gravedad() > 6 && p.gravedad() <= 9) || ( UCI.llena() && p.gravedad() != 0) )
-            PLANTA.insertar(p,PLANTA.fin());
-        else 
-            if(!MORGUE.llena() && !p.gravedad())
-                MORGUE.insertar(p,MORGUE.fin());
+    if ((p.gravedad() > 0 && p.gravedad() < 6) && !uciLLena())
+    {
+        UCI.insertar(p, UCI.fin());
+        --uci_;
+    }
+    else if ((p.gravedad() > 6 && p.gravedad() <= 9) || (uciLLena() && p.gravedad() != 0))
+    {
+        PLANTA.insertar(p, PLANTA.fin());
+        --planta_;
+    }
+    else if (!morgueLLena() && !p.gravedad())
+    {
+        MORGUE.insertar(p, MORGUE.fin());
+        --morgue_;
+    }
 }
 
-
-
-void Hospital::alta(const Paciente& x)
+void Hospital::alta(const Paciente &x)
 {
-    PLANTA.eliminar(PLANTA.buscar(x)) ;
+    PLANTA.eliminar(PLANTA.buscar(x));
 }
 
 void Hospital::UCIS() const
 {
-    typename Lista<Paciente>::posicion p ;
+    typename Lista<Paciente>::posicion p;
 
-    for(p = UCI.primera() ; p != UCI.fin(); p = UCI.siguiente(p))
+    for (p = UCI.primera(); p != UCI.fin(); p = UCI.siguiente(p))
     {
-        std::cout << "Pacientes en UCI: " << UCI.elemento(p).nombre() << std::endl ; 
+        std::cout << "Pacientes en UCI: " << UCI.elemento(p).nombre() << std::endl;
     }
 }
 
 void Hospital::PLANTAS() const
 {
-    typename Lista<Paciente>::posicion p ;
+    typename Lista<Paciente>::posicion p;
 
-    for(p = PLANTA.primera() ; p != PLANTA.fin(); p = PLANTA.siguiente(p))
+    for (p = PLANTA.primera(); p != PLANTA.fin(); p = PLANTA.siguiente(p))
     {
-        std::cout << "Pacientes en Planta: " << PLANTA.elemento(p).nombre() << std::endl ; 
+        std::cout << "Pacientes en Planta: " << PLANTA.elemento(p).nombre() << std::endl;
     }
 }
 
 void Hospital::muerte()
 {
-    typename Lista<Paciente>::posicion p ;
+    typename Lista<Paciente>::posicion p;
 
-    for(p = MORGUE.primera() ; p != MORGUE.fin(); p = MORGUE.siguiente(p))
+    for (p = MORGUE.primera(); p != MORGUE.fin(); p = MORGUE.siguiente(p))
     {
-        MORGUE.eliminar(p) ; 
+        MORGUE.eliminar(p);
     }
 }
 
 void Hospital::gravedad_determinada(int h) const
 {
-    typename Lista<Paciente>::posicion p ;
-    typename Lista<Paciente>::posicion u ;
-    typename Lista<Paciente>::posicion m ;
-     
-     if(h > 0 && h < 6)
-     {
-        for(p = PLANTA.primera() ; p != PLANTA.fin(); p = PLANTA.siguiente(p))
+    typename Lista<Paciente>::posicion p;
+    typename Lista<Paciente>::posicion u;
+    typename Lista<Paciente>::posicion m;
+
+    if (h > 0 && h < 6)
+    {
+        for (p = PLANTA.primera(); p != PLANTA.fin(); p = PLANTA.siguiente(p))
         {
-           if(PLANTA.elemento(p).gravedad() == h)
-           {
-               std::cout << PLANTA.elemento(p).nombre() << std::endl ; 
-           }
-        }     
-     }
-     else
-     {
-         if(h >= 6 && h < 10)
-         {
-            for(u = UCI.primera() ; u != UCI.fin(); u = UCI.siguiente(u))
+            if (PLANTA.elemento(p).gravedad() == h)
             {
-                if(UCI.elemento(u).gravedad() == h)
+                std::cout << PLANTA.elemento(p).nombre() << std::endl;
+            }
+        }
+    }
+    else
+    {
+        if (h >= 6 && h < 10)
+        {
+            for (u = UCI.primera(); u != UCI.fin(); u = UCI.siguiente(u))
+            {
+                if (UCI.elemento(u).gravedad() == h)
                 {
-                    std::cout << UCI.elemento(u).nombre() << std::endl ; 
+                    std::cout << UCI.elemento(u).nombre() << std::endl;
                 }
-            }    
-         }
-         else
-         {
-             if(h == 0)
-             {
-                for(u = UCI.primera() ; u != UCI.fin(); u = UCI.siguiente(u))
+            }
+        }
+        else
+        {
+            if (h == 0)
+            {
+                for (u = UCI.primera(); u != UCI.fin(); u = UCI.siguiente(u))
                 {
-                                        
-                    std::cout << UCI.elemento(u).nombre() << std::endl ; 
-                    
-                }    
-             }
-         }
-     }
+
+                    std::cout << UCI.elemento(u).nombre() << std::endl;
+                }
+            }
+        }
+    }
+}
+
+bool Hospital::uciLLena()
+{
+    return (uci_ == 0);
+}
+
+bool Hospital::plantaLLena()
+{
+    return (planta_ == 0);
+}
+
+bool Hospital::morgueLLena()
+{
+    return (morgue_ == 0);
+}
+
+bool operator==(const Paciente &X, const Paciente &Y)
+{
+    return X.id() == Y.id();
 }
